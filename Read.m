66c94402fc79e0a773417d5876307e3f0dbc5af8
H@M <631379593@qq.com>
2017-04-18 15:43:53 +0800

    Read.m

---
+++ Read.m
@@ -0,0 +1,16 @@
+## install node first
+## cd package/static
+npm install -g babel-cli
+npm install babel-preset-es2015 --save
+npm install babel-preset-react --save
+babel --presets es2015,react --watch js/ --out-dir lib/
+
+
+
+
+
+
+
+## install bower first
+## cd package/static
+bower install
